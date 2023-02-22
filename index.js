2b95658e6a0ab38a22acd86d03fced3c0caf0df0
WebDev717 <blackhorse.cryptodev@gmail.com>
2023-02-22 18:06:17 +0200

    onetime icon

--- index.js
+++ index.js
@@ -1,28 +1,94 @@
-const express = require('express');
-const webpush = require('web-push');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const request = require('request');
+
+require('dotenv').config();
+const API_KEY = process.env.onesignal_api_key;
+const ONESIGNAL_APP_ID = "58565960-d135-41b4-b90b-a139f385b537";
+const BASE_URL = "https://onesignal.com/api/v1";
+
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
 
-app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "client")))
+/**
+ * OPTIONS BUILDER
+ * @param {string} method
+ * @param {string} path
+ * @param {object} body
+ * @returns {object} options
+ */
+const optionsBuilder = (method, path, body) => {
+    return {
+        method,
+        'url': `${BASE_URL}/${path}`,
+        'headers': {
+            'Content-Type': 'application/json',   
+            'Authorization': `Basic ${API_KEY}`,
+        },
+        body: body ? JSON.stringify(body) : null,
+    };
+}
 
-const publicVapidKey = "BOd2EQ8LTe3KAgMX9lWwTlHTRzv1Iantw50Mw6pUnsNr3pcxl8iglUs-YlQEQLo4UbJk9oyXs_BxgyAe0TCqKME";
-const privateVapidKey = "4AoSsRHFaHv0Fupd2NRtrungJF2jkqgccTu-WEc781w";
+/**
+ * CREATE A PUSH NOTIFICATION
+ * method: POST
+ * Postman: https://www.postman.com/onesignaldevs/workspace/onesignal-api/request/16845437-c4f3498f-fd80-4304-a6c1-a3234b923f2c
+ * API Reference: https://documentation.onesignal.com/reference#create-notification
+ * path: /notifications
+ * @param {object} body
+ */
 
-webpush.setVapidDetails("mailto:test@test.com", publicVapidKey, privateVapidKey);
+const createNotication = (body) => {
+    const options = optionsBuilder("POST","notifications", body);
+    console.log(options);
+    request(options, (error, response) => {
+        if (error) throw new Error(error);
+        console.log(response.body);
+        viewNotifcation(JSON.parse(response.body).id);
+    });
+}
 
-app.post('/subscribe', (req, res) => {
-    const subscription = req.body;
-    res.status(201).json({});
-    const payload = JSON.stringify({ title: "Hello World", body: "This is your first push notification" });
-    webpush.sendNotification(subscription, payload).catch(console.log);
-})
+/**
+ * VIEW NOTIFICATION
+ * method: GET
+ * Postman: https://www.postman.com/onesignaldevs/workspace/onesignal-api/request/16845437-6c96ecf0-5882-4eac-a386-0d0cabc8ecd2
+ * API Reference: https://documentation.onesignal.com/reference#view-notification
+ * path: /notifications/{notification_id}?app_id=${ONE_SIGNAL_APP_ID}
+ * @param {string} notificationId
+ */
+const viewNotifcation = (notificationId) => {
 
-const PORT = 5000;
+    const path = `notifications/${notificationId}?app_id=${ONESIGNAL_APP_ID}`;
+    const options = optionsBuilder("GET", path);
+    request(options, (error, response)=> {
+        if (error) throw new Error(error);
+        console.log(response.body);
+    });
+}
 
-app.listen(PORT, () => {
-    console.log("Server started on port " + PORT);
-})
+/**
+ * RUN THE NODE JS APP
+ */
+const body = {
+    app_id: ONESIGNAL_APP_ID,
+    included_segments: ['Subscribed Users'],
+    data: {
+      foo: 'bar',
+    },
+    contents: {
+      en: 'Sample Push Message',
+    },
+};
+
+app.get("/", async (req, res) => {
+    createNotication(body);
+});
+
+
+app.listen(6969, () => {
+    console.log("start");
+  });
